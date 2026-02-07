63a912dac89d3408735ac44c3249dd2a54dd8890
jubaer1141 <mdjubayer1141@gmail.com>
2026-02-08 03:27:51 +0600

    InitialCommit

---
+++ operators/assingment.py
@@ -0,0 +1,29 @@
+# =  += -=  *=  /=  %=  //=  **=  
+# used to assign and upadate values
+
+x=10
+print("x=",x) 
+#assign x=10
+
+x += 5
+print("x += 5 :", x)  
+#updated x=10
+#then 10+5=15
+
+x -= 3
+print("x -= 3 :", x)
+#update x=15
+#then 15-3=12
+
+x *= 2 
+print("x *= 2: ", x)
+#update x=12
+#then 12*2=24
+
+x /= 4 
+print("x /= 4: ", x)
+#....
+
+x %= 6
+print("X %= 6", x)
+#......
---
+++ operators/comparison.py
@@ -0,0 +1,11 @@
+# ==  !=  >  <  >=  <=
+
+x=10
+y=5
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x <= y)
+print(x >= y)
