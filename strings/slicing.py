7f8141bfa8a2e520537794ed8cb63653ccc903e8
jubaer1141 <mdjubayer1141@gmail.com>
2026-02-09 01:13:59 +0600

    Update Slicing with step

--- strings/slicing.py
+++ strings/slicing.py
@@ -9,4 +9,18 @@ print(string[3:])      # hon
 print(string[-4:-1])   # tho
 
 
+
+#(string[start : End : step])
+
 print(string[0:6:2])   #Pto
+#here start index 0------> start from o index
+#End 6 -----------> Go up to 6 index ( 6 is not include)
+#step 2 -----------> jump 2 step each time and take the value 
+
+
+#another example:
+string = "ABCDEFGHIJKLMNOPWRSTUVWXYZ"
+print(string[1:24:3])  #output "BEHKNWTWZ"
+#BEHKNWTWZ
+#start from 1 means B
+#End at 24 means X
