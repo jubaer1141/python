825a7c68a4c31ba7ffc4c1dc96dee86df5e539ff
jubaer1141 <mdjubayer1141@gmail.com>
2026-02-08 18:35:45 +0600

    Intial Commit

---
+++ strings/escape_characters.py
@@ -0,0 +1,8 @@
+print("Hello\nWorld")     # new line
+print("Name\tAge")        # tab
+print("C:\\Python\\Day4") # backslash
+print("He said \"Hello\"")
+print('It\'s my book')
+
+
+print(r"C:\Python\Day4\strings")
