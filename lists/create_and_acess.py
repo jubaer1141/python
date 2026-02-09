43231f67e15ba525b0b8e35d96ff5d0be2c10883
jubaer1141 <mdjubayer1141@gmail.com>
2026-02-09 13:16:30 +0600

    Initial Commit

---
+++ lists/create_and_acess.py
@@ -0,0 +1,30 @@
+#create 
+
+numbers = [10, 20 , 30, 40]
+print (numbers)
+
+fruits = ['apple', 'orange', 'banana', 'mango']
+print(fruits)
+
+
+#access
+
+numbers = [10, 20, 30, 40]
+
+print(numbers[0])    #10
+print(numbers[2])    #30
+print(numbers[-1])   #40
+
+print(fruits[0:3]) #['apple', 'orange', 'banana']
+# exclude end value
+
+
+
+
+#lenght
+
+numbers = [10, 20, 30, 40]
+print(len(numbers))  #4  
+
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))  #5
