7a7132b45b4fe9be6aa9f6c32e0ef5fb6cbdd3a8
jubaer1141 <mdjubayer1141@gmail.com>
2026-02-21 14:07:21 +0600

    initial commit

---
+++ decision_making/basic_if_else.py
@@ -0,0 +1,28 @@
+# if_else.py
+
+age = 20
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are not an adult")
+
+
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+
+
+
+username = "jubaer"
+password = "1234"
+
+if username == "jubaer" and password == "1234":
+    print("Login successful")
+else:
+    print("Invalid login")
